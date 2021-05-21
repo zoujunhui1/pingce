2dc44700e51e028ff228697408378cecfe996bf5
zjh <zoujunhui@bilibili.co>
2021-05-21 15:59:22 +0800

    init

---
+++ app/Http/Controllers/EvaluateController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class EvaluateController extends Controller
+{
+    public function addProduct(){
+        dd(111);
+    }
+
+}
---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EvaluateController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/test1', function () {
+    return view('welcome');
+});
+
+Route::post('/evaluate/add', [EvaluateController::class, 'addProduct']);
