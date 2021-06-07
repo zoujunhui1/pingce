b0d88a59c09a8bb0fa3ad7e7c069d62d2e1594ad
zjh <zoujunhui@bilibili.co>
2021-06-07 18:54:03 +0800

    fix

---
+++ app/Http/Middleware/EnableCrossRequestMiddleware.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class EnableCrossRequestMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $response = $next($request);
+        $origin = $request->server('HTTP_ORIGIN') ? $request->server('HTTP_ORIGIN') : '';
+        $allow_origin = [
+            '*'
+        ];
+        if (in_array($origin, $allow_origin)) {
+            $data = [
+                'Access-Control-Allow-Origin'      => $origin,
+                'Access-Control-Allow-Headers'     => 'Origin, Content-Type, Cookie, X-CSRF-TOKEN, Accept, Authorization, X-XSRF-TOKEN',
+                'Access-Control-Expose-Headers'    => 'Authorization, authenticated',
+                'Access-Control-Allow-Methods'     => 'GET, POST, PATCH, PUT, OPTIONS',
+                'Access-Control-Allow-Credentials' => 'true'
+            ];
+            $response->headers->add($data);
+//            $response->header('Access-Control-Allow-Origin', $origin);
+//            $response->header('Access-Control-Allow-Headers', 'Origin, Content-Type, Cookie, X-CSRF-TOKEN, Accept, Authorization, X-XSRF-TOKEN');
+//            $response->header('Access-Control-Expose-Headers', 'Authorization, authenticated');
+//            $response->header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, OPTIONS');
+//            $response->header('Access-Control-Allow-Credentials', 'true');
+        }
+        return $response;
+    }
+
+}
