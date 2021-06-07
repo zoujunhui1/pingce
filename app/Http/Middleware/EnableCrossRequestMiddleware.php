7287618cf05065c52256adb7930d7270ab15c80c
zjh <zoujunhui@bilibili.co>
2021-06-07 19:35:16 +0800

    fix

--- app/Http/Middleware/EnableCrossRequestMiddleware.php
+++ app/Http/Middleware/EnableCrossRequestMiddleware.php
@@ -17,26 +17,20 @@ class EnableCrossRequestMiddleware
     public function handle($request, Closure $next)
     {
         $response = $next($request);
-        $origin = $request->server('HTTP_ORIGIN') ? $request->server('HTTP_ORIGIN') : '';
-        $allow_origin = [
-            '*',
-            'http://localhost:8090'
+//        $origin = $request->server('HTTP_ORIGIN') ? $request->server('HTTP_ORIGIN') : '';
+//        $allow_origin = [
+//            '*',
+//            'http://localhost:8090',
+//            'http://150.109.150.224:8090'
+//        ];
+        $data = [
+            'Access-Control-Allow-Origin'      => '*',
+            'Access-Control-Allow-Headers'     => 'Origin, Content-Type, Cookie, X-CSRF-TOKEN, Accept, Authorization, X-XSRF-TOKEN',
+            'Access-Control-Expose-Headers'    => 'Authorization, authenticated',
+            'Access-Control-Allow-Methods'     => 'GET, POST, PATCH, PUT, OPTIONS',
+            'Access-Control-Allow-Credentials' => 'true'
         ];
-        if (in_array($origin, $allow_origin)) {
-            $data = [
-                'Access-Control-Allow-Origin'      => '*',
-                'Access-Control-Allow-Headers'     => 'Origin, Content-Type, Cookie, X-CSRF-TOKEN, Accept, Authorization, X-XSRF-TOKEN',
-                'Access-Control-Expose-Headers'    => 'Authorization, authenticated',
-                'Access-Control-Allow-Methods'     => 'GET, POST, PATCH, PUT, OPTIONS',
-                'Access-Control-Allow-Credentials' => 'true'
-            ];
-            $response->headers->add($data);
-//            $response->header('Access-Control-Allow-Origin', $origin);
-//            $response->header('Access-Control-Allow-Headers', 'Origin, Content-Type, Cookie, X-CSRF-TOKEN, Accept, Authorization, X-XSRF-TOKEN');
-//            $response->header('Access-Control-Expose-Headers', 'Authorization, authenticated');
-//            $response->header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, OPTIONS');
-//            $response->header('Access-Control-Allow-Credentials', 'true');
-        }
+        $response->headers->add($data);
         return $response;
     }
 
