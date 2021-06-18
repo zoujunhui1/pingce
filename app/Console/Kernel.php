ca1a1002f22f5aaddae5fab00b99cb333c99ce5f
zjh <zoujunhui@bilibili.co>
2021-06-18 20:16:23 +0800

    fix

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,6 +26,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
+         $schedule->command('create:qr_code')->everyMinute();
     }
 
     /**
